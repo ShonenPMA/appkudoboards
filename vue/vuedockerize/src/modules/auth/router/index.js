d06e23d2927bcba20d3b7ad7b881bc1c3287685b
Jhon Ayrton Saavedra Castañeda <ayrtonsc6@gmail.com>
2021-11-15 02:40:05 -0500

    📗 feat: logout, unauth guard

--- vue/vuedockerize/src/modules/auth/router/index.js
+++ vue/vuedockerize/src/modules/auth/router/index.js
@@ -1,15 +1,18 @@
 import isAuthenticatedGuard from './auth-guard';
+import isUnauthenticatedGuard from './unauth-guard';
 export default {
     name: 'auth',
     component: () => import(/* webpackChunkName: "auth" */ '@/modules/auth/layouts/AuthLayout.vue'),
     children: [
         {
             path: '',
+            beforeEnter: [ isUnauthenticatedGuard ],
             name: 'login',
             component: () => import(/* webpackChunkName: "auth-login" */ '@/modules/auth/views/Login.vue'),
         },
         {
             path: '/register',
+            beforeEnter: [ isUnauthenticatedGuard ],
             name: 'register',
             component: () => import(/* webpackChunkName: "auth-login" */ '@/modules/auth/views/Register.vue'),
         },
