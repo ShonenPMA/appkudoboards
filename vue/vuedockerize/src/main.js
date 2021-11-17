4e0953b05994eea89fbbefddaf4d6cb5c247bb01
Jhon Ayrton Saavedra Castañeda <ayrtonsc6@gmail.com>
2021-11-16 22:03:28 -0500

    📗 feat: basic config websocket

--- vue/vuedockerize/src/main.js
+++ vue/vuedockerize/src/main.js
@@ -30,6 +30,23 @@ library.add(
     faUserTie,
 )
 
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.VUE_APP_WEBSOCKET_APP_KEY,
+    cluster: process.env.VUE_APP_WEBSOCKET_APP_CLUSTER,
+    wsHost: process.env.VUE_APP_WEBSOCKET_WS_HOST,
+    wsPort: process.env.VUE_APP_WEBSOCKET_WS_PORT,
+    forceTLS: process.env.VUE_APP_WEBSOCKET_FORCE_TLS,
+    encrypted: process.env.VUE_APP_WEBSOCKET_ENCRYPTED,
+    disabledStats: process.env.VUE_APP_WEBSOCKET_DISABLED_STATS,
+    // authEndpoint: 'broadcasting/auth' si el socket no jala de raiz 
+});
+
+
 createApp(App)
     .use(store)
     .use(router)
